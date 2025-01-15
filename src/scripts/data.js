7f0349835d16900c3d2d37d22c38a2024e3d1c3c
marshall-bits <kz_@hotmail.es>
2025-01-15 12:47:52 +0100

    add js for the projects

---
+++ src/scripts/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    image: {
+      src: "./assets/project-3.png",
+      alt: "Captura de pantalla de Dittopedia",
+    },
+    title: "Dittopedia",
+    tech: "MongoDB, Express, Angular, Node.js",
+    description:
+      "Aplicación full-stack MEAN para compartir recursos para developers. CRUD de recursos y usuarios.",
+    link: "https://github.com/Marshall-Bits?tab=repositories&q=dittopedia&type=&language=&sort=",
+  },
+  {
+    image: {
+      src: "./assets/project-1.png",
+      alt: "Captura de pantalla de Unity 2D",
+    },
+    tech: "Unity, C#",
+    title: "Unity 2D",
+    description:
+      "Videotutoriales de desarrollo de videojuegos en Unity 2D. Aprende a crear videojuegos desde cero con Unity y C#.",
+    link: "https://www.youtube.com/@marshallbits",
+  },
+  {
+    image: {
+      src: "./assets/project-2.png",
+      alt: "Captura de pantalla de Family events",
+    },
+    tech: "MongoDB, Express, React, Node.js",
+    title: "Family events",
+    description:
+      "Aplicación full-stack MERN para la gestión de eventos. CRUD de eventos y usuarios.",
+    link: "https://github.com/Marshall-Bits?tab=repositories&q=family-events&type=&language=&sort=",
+  },
+  {
+    image: {
+      src: "./assets/project-4.png",
+      alt: "Captura de pantalla de Sudo space traveler",
+    },
+    tech: "HTML, CSS, JavaScript",
+    title: "Sudo space traveler",
+    description:
+      "Uno de mis proyectos de bootcamp. Juego de sudoku con temática espacial.",
+    link: "https://github.com/Marshall-Bits/sudoku--project",
+  },
+];
